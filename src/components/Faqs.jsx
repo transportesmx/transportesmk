7761e85181a0876c7d59818cef4841c58f224858
Hyzmael Zerrano <31554724+hyzmael88@users.noreply.github.com>
2025-01-30 18:24:35 -0600

    Agregar funcionalidad de acordeón en el componente Faqs y mejorar el diseño de la sección de preguntas frecuentes

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 const faqData = [
   {
@@ -25,38 +26,63 @@ const faqData = [
 ];
 
 export default function Faqs() {
+  const [selectedFaq, setSelectedFaq] = useState(null);
+
+  const toggleFaq = (index) => {
+    setSelectedFaq(selectedFaq === index ? null : index);
+  };
+
   return (
-    <div className="relative bg-cover bg-center h-[1200px] md:h-[1050px] px-6 sm:px-10 lg:px-16 py-10" style={{ backgroundImage: 'url(/assets/images/faqs.png)' }}>
+    <div className="relative bg-cover bg-center h-[900px] md:h-[750px] px-6 sm:px-10 lg:px-16 py-10 flex flex-col justify-center" style={{ backgroundImage: 'url(/assets/images/faqs.png)' }}>
       <h2 className="text-4xl font-bold text-center lg:text-left text-white mb-10">Preguntas Frecuentes</h2>
-      <div className='flex justify-around gap-4'>
+      <div className='flex items-center justify-around gap-4'>
 {/* izquierda */}
       <div className="w-2/3 mx-auto">
         {faqData.map((faq, index) => (
           <motion.div
-          key={index}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: index * 0.2 }}
-          className="bg-gray-100/15 p-5 mb-4 rounded-lg shadow-lg"
+            key={index}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+            className="bg-gray-100/15 p-5 mb-4 rounded-lg shadow-lg"
           >
-            <motion.div
-              initial={{ height: 0 }}
-              animate={{ height: 'auto' }}
-              transition={{ duration: 0.3 }}
-              className="border-b border-gray-300"
-              >
+            <div className="flex justify-between items-center border-b border-gray-300 cursor-pointer" onClick={() => toggleFaq(index)}>
               <h3 className="text-[22px] font-semibold text-white">{faq.question}</h3>
-              <p className="text-white mt-2 text-[18px]">{faq.answer}</p>
-            </motion.div>
+              <span className="text-white text-[22px]">{selectedFaq === index ? '-' : '+'}</span>
+            </div>
+            {selectedFaq === index && (
+              <motion.div
+                initial={{ height: 0 }}
+                animate={{ height: 'auto' }}
+                transition={{ duration: 0.3 }}
+                className="overflow-hidden"
+              >
+                <p className="text-white mt-2 text-[18px]">{faq.answer}</p>
+              </motion.div>
+            )}
           </motion.div>
         ))}
       </div>
       <div>
 
       </div>
-      <div className='hidden lg:block   bg-red-500 h-[800px] w-1/3'>
+      <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay:  0.2 }}
+            className="hidden   h-[435px] w-1/3 bg-gray-100/15 p-5 mb-4 rounded-lg shadow-lg lg:flex flex-col items-center justify-center"
+          >
+            <img src="/assets/icons/mensaje.png" alt="FAQs" className="w-[67px] h-[68px] object-cover rounded-lg" />
+            <h3 className='text-white text-[22px] font-semibold text-center mt-4'>
+            ¿Tienes mas preguntas?
+            </h3>
+            <p className='text-[16px] text-center mt-4'>Estamos para apoyarte en todo momento por favor da click en el boton para comenzar a chatear.</p>
+            <button className='px-24 py-2  bg-white/50 hover:bg-gray-700 rounded-lg transition mt-8 font-bold'>
+              Chatear
+            </button>
 
-      </div>
+          </motion.div>
+
         </div>
     </div>
   );
