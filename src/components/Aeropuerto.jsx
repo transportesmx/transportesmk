418def073564c2b417b40ff2444408081c0d07a1
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2025-02-03 17:37:47 -0600

    Actualizar componente Aeropuerto: ajustar estilos de alineación y mejorar la presentación del contenido Actualizar componente Testimonios: corregir imágenes de usuario y mejorar el estilo de presentación

--- src/components/Aeropuerto.jsx
+++ src/components/Aeropuerto.jsx
@@ -6,7 +6,7 @@ function Aeropuerto() {
     <div className='h-[950px]   '
     style={{backgroundImage: 'url(/assets/images/aeropuerto.png)', backgroundSize: 'cover', backgroundPosition: 'center'}}
     >
-      <div className='max-w-7xl mx-auto px-6 sm:px-10 lg:px-16 py-10 h-full flex flex-col justify-center lg:justify-start items-center lg:items-start'>
+      <div className='max-w-7xl mx-auto px-6 sm:px-10 lg:px-16 py-10 h-full flex flex-col justify-center  items-center lg:items-start'>
 
       <h1 className='text-[40px] lg:text-[50px] leading-[45px] lg:leading-[50px] font-bold mb-2 text-center'>Traslado a Aeropuerto</h1>
       <p className='text-lg lg:text-xl text-center px- mb-6'>Servicio personalizado, rápido y confiable.</p>
--- src/components/Testimonios.jsx
+++ src/components/Testimonios.jsx
@@ -85,7 +85,7 @@ const testimonios = [
     description:
       "El tour por Guanajuato fue espectacular. Aprendimos mucho sobre la historia del lugar y disfrutamos cada parada. Excelente transporte y organización.",
         imagen: "/assets/images/testimonios/10.png",
-      usuario:"/assets/images/testimonios/usuario10.png",
+      usuario:"/assets/images/testimonios/usuario11.png",
         servicio: "Tours (Guanajuato)",
   },
   {
@@ -93,7 +93,7 @@ const testimonios = [
     description:
       "Mi familia y yo usamos el servicio de traslado desde el aeropuerto y fue excelente. Puntualidad, seguridad y comodidad garantizadas.",
         imagen: "/assets/images/testimonios/11.png",
-      usuario:"/assets/images/testimonios/usuario11.png",
+      usuario:"/assets/images/testimonios/usuario10.png",
         servicio: "Traslado a Aeropuerto",
   },
   {
@@ -161,7 +161,7 @@ const Testimonios = () => {
               
             <div className="bg-white/15 rounded-lg shadow-lg  rounded-tl-[8px] rounded-tr-[48px] rounded-br-[8px] rounded-bl-[48px] h-[268px] w-[274px] relative">
 
-            <Image src={tour.usuario} alt={tour.usuario} width={72} height={72} className="w-[72px] h-[72px] absolute left-0 right-0 mx-auto -top-10 " />
+            <Image src={tour.usuario} alt={tour.usuario} width={72} height={72} className="w-[72px] h-[72px] absolute left-0 right-0 mx-auto -top-10 rounded-full" />
               
               <div className="p-4 text-center">
               <h3 className="text-[16px] font-bold mt-6">{tour.nombre}</h3>
