037e8b748200abeaba1d2e4ffd18bf40f494e543
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2025-02-04 19:23:35 -0600

    Agregar metadatos SEO y favicon en la página de inicio para mejorar la visibilidad y presentación

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ import Hero from "@/components/Hero";
 import Testimonios from "@/components/Testimonios";
 import Tours from "@/components/Tours";
 import BodasModal from "@/components/BodasModal"; // Asegúrate de importar el componente del modal
+import Head from "next/head";
+
 
 export default function Home() {
   const [isBodasModalVisible, setIsBodasModalVisible] = useState(false);
@@ -24,6 +26,40 @@ export default function Home() {
 
   return (
     <Fragment>
+       <Head>
+      <meta name="theme-color" content="#000000" />
+      <title>Transportes MX - Traslados Seguros y Confiables</title>
+      <meta
+        name="description"
+        content="Transportes MX ofrece traslados seguros y cómodos en San Miguel de Allende, Querétaro, Guanajuato y más. Transporte ejecutivo, traslados a aeropuertos, chofer privado y tours exclusivos. Reserva con nosotros y viaja con comodidad y seguridad."
+      />
+      <meta
+        name="keywords"
+        content="Transporte, Traslados, Aeropuerto, Bodas, Chofer Privado, Ejecutivo, Tours, San Miguel de Allende, Guanajuato, Querétaro, Viajes, Rentas, Transporte Premium, Transportes MX"
+      />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:title" content="Transportes MX - Traslados Seguros y Confiables" />
+      <meta
+        property="og:description"
+        content="Viaja con seguridad y comodidad con Transportes MX. Servicios de traslado a aeropuertos, bodas, ejecutivos, chofer privado y tours en San Miguel de Allende, Querétaro y Guanajuato."
+      />
+      <meta property="og:image" content="/assets/images/ogimage.jpg" />
+      <meta property="og:url" content="https://www.transportesmx.com" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Transportes MX - Traslados Seguros y Confiables" />
+      <meta
+        name="twitter:description"
+        content="Reserva tu transporte con Transportes MX y viaja seguro. Servicios de chofer privado, aeropuertos, bodas y tours."
+      />
+      <meta name="twitter:image" content="/assets/images/ogimage.jpg" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
       <Hero />
       <About />
       <Aeropuerto />
